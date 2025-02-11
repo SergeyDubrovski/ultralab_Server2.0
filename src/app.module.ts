ca5481971bf664163798b2b5da181a7b284460df
SergDubrovski <sovelectron@gmail.com>
2025-02-11 10:55:17 +0300

    11.02.11.44

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { SequelizeModule } from "@nestjs/sequelize";
 
-@Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+@Module( {
+    controllers: [AppController],
+    providers:[AppService],
+    imports: [
+        SequelizeModule.forRoot({
+          dialect: 'postgres',
+          host: 'localhost',
+          port: 5432,
+          username: 'postgres',
+          password: 'Serg1990',
+          database: 'ultralab2',
+          models: [],
+          autoLoadModels: true
+        }),
+      ],
 })
-export class AppModule {}
+export class AppModule {
+
+}
